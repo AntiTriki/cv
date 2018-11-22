d15d2253ae092ac3890667810bc3ec380ce018b6
AntiTriki <ne.i.on@hotmail.com>
2018-11-22 15:32:19 -0400

    Revert "Revert "First commit""
    This reverts commit c2b0039271350f45508c28723565509ed16806fc.

---
+++ database/migrations/2018_11_13_134752_create_roles_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->softDeletes();
+            $table->integer('enterprise_id')->unsigned();
+            $table->foreign('enterprise_id')->references('id')->on('enterprises')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('roles');
+    }
+}
