9cdc478b1d7cff485ae2ec74495b25bfef394205
AntiTriki <ne.i.on@hotmail.com>
2018-12-20 18:05:44 -0400

    Vista de login chequeado
    sdsd

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,74 +1,76 @@
 @extends('layouts.app')
 
+@section('body-class','signup-page')
 @section('content')
-    <br>
-    <br>
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+    <div class="page-header header-filter" style="background-image: url({{asset('img/city.jpg')}}); background-size: cover; background-position: top center;">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="card-header card-header-primary text-center">
+                            <h4 class="card-title">Acceso</h4>
+                        </div>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+                        <div class="card-body">
+                            <form autocomplete="off" method="POST" action="{{ route('login') }}">
+                                @csrf
 
-                        <div class="form-group bmd-form-group">
-                            <label for="email" class=" col-form-label bmd-label-floating ">{{ __('E-Mail Address') }}</label>
+                                <div class="input-group">
+
+                                    <div class="input-group-prepend">
+                                        <span class="input-group-text">
+                                          <i class="material-icons">mail</i>
+                                        </span>
+                                    </div>
 
 
-                                <input id="email" type="email" class=" form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+                                    <input id="email" type="email" class=" form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" placeholder="Email..." name="email"  required >
 
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
+                                    @if ($errors->has('email'))
+                                        <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
-                                @endif
+                                    @endif
 
-                        </div>
+                                </div>
+                                <br>
+                                 <div class="input-group">
+                                        <div class="input-group-prepend">
+                                            <span class="input-group-text">
+                                              <i class="material-icons">lock_outline</i>
+                                            </span>
+                                        </div>
+                                        <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" placeholder="Password..." name="password" required>
 
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
+                                        @if ($errors->has('password'))
+                                            <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
-                                @endif
-                            </div>
-                        </div>
+                                        @endif
+                                 </div>
 
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
+
+
+
+                                <div class="row">
+                                    <div class="col-md-12 form-group text-center mb-0">
+                                        <button type="submit" class="btn btn-primary btn-link btn-wd btn-lg">
+                                            {{ 'ENTRAR' }}
+                                        </button>
+
+
+                                    </div>
                                 </div>
-                            </div>
+                            </form>
                         </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    {{ __('Forgot Your Password?') }}
-                                </a>
-                            </div>
-                        </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+         </div>
+
+
 
 
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,91 +2,94 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <title>{{ config('app.name', 'HP') }}</title>
-
-
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+    <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
     <link href="{{ asset('css/material-kit.min.css?v=2.0.5') }}" rel="stylesheet">
     <link href="{{ asset('css/demo.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
-    <!-- Scripts -->
-
-    <script src="{{ asset('js/core/jquery.min.js') }}" ></script>
-    <script src="{{ asset('js/core/popper.min.js') }}" ></script>
-    <script src="{{ asset('js/core/bootstrap-material-design.min.js') }}" ></script>
 
 
-    <script src="{{ asset('js/plugins/moment.min.js') }}" ></script>
-    <script src="{{ asset('js/plugins/bootstrap-datetimepicker.js') }}" ></script>
-    <script src="{{ asset('js/plugins/nouislider.min.js') }}" ></script>
-    <script async defer src="https://buttons.github.io/buttons.js"></script>
 
-    <script src="{{ asset('js/material-kit.js?v=2.0.5') }}" ></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+
 
 
 </head>
-<body class="profile-page sidebar-collapse">
+<body class="@yield('body-class')">
+<nav class="navbar navbar-transparent navbar-color-on-scroll fixed-top navbar-expand-lg" color-on-scroll="100" id="sectionsNav">
+    <div class="container">
+        <div class="navbar-translate">
+            <a class="navbar-brand" href="{{ url('/') }}">
+                {{  'HP' }}
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="navbar-toggler-icon"></span>
+                <span class="navbar-toggler-icon"></span>
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+        <div class="collapse navbar-collapse">
+            <ul class="navbar-nav ml-auto">
+                @guest
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                </li>
+                <li class="nav-item">
+                    @if (Route::has('register'))
+                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                    @endif
+                </li>
+                @else
+                    <li class="nav-item">
+                        <a id="navba" class="nav-link " href="#" role="button"  a >
+                            {{ Auth::user()->name }}
+                        </a>
 
-        <nav class="navbar navbar-transparent navbar-color-on-scroll fixed-top navbar-expand-lg"  color-on-scroll="100"  id="sectionsNav">
-            <div class="container">
-                <div class="navbar-translate">
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'HP') }}
-                    </a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="navbar-toggler-icon"></span>
-                        <span class="navbar-toggler-icon"></span>
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                </div>
-                <div class="collapse navbar-collapse">
-                    <ul class="navbar-nav ml-auto">
-                        @guest
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                        </li>
-                        <li class="nav-item">
-                            @if (Route::has('register'))
-                                <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                            @endif
-                        </li>
-                        @else
-                            <li class="nav-item">
-                                <a id="navba" class="nav-link " href="#" role="button"  a >
-                                    {{ Auth::user()->name }}
-                                </a>
 
-
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                    <i class="material-icons">cloud_download</i> {{ __('Logout') }}
-                                </a>
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                    @csrf
-                                </form>
-                            </li>
-                            @endguest
-                    </ul>
-                </div>
-            </div>
-        </nav>
-        <main class="py-5">
+                            <i class="material-icons">cloud_download</i> {{ __('Logout') }}
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </li>
+                    @endguest
+            </ul>
+        </div>
+    </div>
+</nav>
+
+<main class=" wrapper">
             @yield('content')
         </main>
 
 </body>
+        <!-- Scripts -->
+
+        <script src="{{ asset('js/core/jquery.min.js') }}" type="text/javascript"></script>
+        <script src="{{ asset('js/bootstrap.js') }}" type="text/javascript"></script>
+        <script src="{{ asset('js/core/popper.min.js') }}" type="text/javascript"></script>
+        <script src="{{ asset('js/core/bootstrap-material-design.min.js') }}" type="text/javascript"></script>
+
+
+        <script src="{{ asset('js/plugins/moment.min.js') }}" type="text/javascript"></script>
+        <script src="{{ asset('js/plugins/bootstrap-datetimepicker.js') }}" type="text/javascript"></script>
+        <script src="{{ asset('js/plugins/nouislider.min.js') }}" type="text/javascript"></script>
+        <script async defer src="https://buttons.github.io/buttons.js"></script>
+
+        <script src="{{ asset('js/material-kit.js?v=2.0.5') }}" type="text/javascript"></script>
+
 </html>
