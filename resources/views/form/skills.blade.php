b004af34fb1b8b3441ee6a4d7d1d7b5b66604df7
AntiTriki <ne.i.on@hotmail.com>
2019-03-21 09:11:18 -0400

    Update skills.blade.php

--- resources/views/form/skills.blade.php
+++ resources/views/form/skills.blade.php
@@ -48,23 +48,7 @@
                                             </tr>
                                             </thead>
                                             <tbody>
-                                            @foreach ($form->skills as $skills)
-                                                <tr>
-
-                                                    <td  class="contenido">{{$skills->name}}</td>
-
-                                                    <td class="text-right">{{$skills->level}}</td>
-                                                    <td class="td-actions text-right">
-
-                                                        <button type="button" rel="tooltip" class="btn btn-success btn-fab btn-fab-mini btn-round">
-                                                            <i class="material-icons">edit</i>
-                                                        </button>
-                                                        <button type="button" rel="tooltip" class="btn btn-danger btn-fab btn-fab-mini btn-round">
-                                                            <i class="material-icons">close</i>
-                                                        </button>
-                                                    </td>
-                                                </tr>
-                                            @endforeach
+                                            
                                             </tbody>
                                         </table>
                                     </div>
